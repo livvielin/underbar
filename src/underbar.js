3a05990afc5b665049a456225d6020677e831e97
Livvie Lin <livvie.lin@gmail.com>
2015-07-02 20:20:39 -0700

    "Added default iterator to some function"

--- src/underbar.js
+++ src/underbar.js
@@ -201,6 +201,9 @@
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (iterator === undefined) {
+      iterator = _.identity;
+    }
     return !(_.every(collection, function(val) {
       return !iterator(val);
     }));
