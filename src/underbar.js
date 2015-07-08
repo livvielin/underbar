3f433de405864507cc4e5ca41ea5f8e39805b75a
Livvie Lin <livvie.lin@gmail.com>
2015-07-08 11:45:01 -0700

    "Partially implemented delay function, passes first test"

--- src/underbar.js
+++ src/underbar.js
@@ -310,6 +310,8 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    setTimeout(function() { func(args); }, wait);
   };
 
 
